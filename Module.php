2a70eb1f548f42bef8659b931551d348bad9a352
Steven Bühner <buehner@me.com>
2016-02-29 00:45:55 +0100

    Init

---
+++ Module.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendBootstrapView for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+namespace ZendBootstrapView;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\Mvc\MvcEvent;
+
+class Module implements AutoloaderProviderInterface {
+
+	public function getAutoloaderConfig() {
+		return array();
+	}
+
+	public function getConfig() {
+		return include __DIR__ . '/config/module.config.php';
+	}
+
+	public function onBootstrap(MvcEvent $e) {
+	}
+
+}
---
+++ config/module.config.php
@@ -0,0 +1,8 @@
+<?php
+return array( 
+		'view_manager' => array( 
+				'template_path_stack' => array( 
+						'ZendBootstrapView' => __DIR__ . '/../view' 
+				) 
+		) 
+);
